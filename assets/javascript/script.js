47fd7e510544c943941388ba1e5a88a023682696
Crystal Ly <crystal.m.ly@gmail.com>
2019-12-16 20:20:52 -0600

    added some functions to show/hide divs when you click on buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -61,58 +61,108 @@ var questions = [
     }
 ];
 
-var questionTxtEl = $("#questionTxt");
-var answerBtn1 = $("#choice1");
-var answerBtn2 = $("#choice2");
-var answerBtn3 = $("#choice3");
-var answerBtn4 = $("#choice4");
-var feedbackEl = $("#feedback");
-var getStartedBtn = $("#getStarted");
+$(document).ready(function () {
+    var welcomeBox = $("#welcome");
+    var questionBox = $("#question");
+    var endingScoreBox = $("#endingScoreBox");
+    var highScoresBox = $("#highScores");
 
-//when I click on getStartedBtn, the page will shuffle the questions, then load each question 1 by 1 until all 10 are answered. 
-//It will also have a timer of 200 seconds TOTAL, for entire quiz, and will alert, then load the next question.
+    var questionTxtEl = $("#questionTxt");
+    var answerBtn1 = $("#choice1");
+    var answerBtn2 = $("#choice2");
+    var answerBtn3 = $("#choice3");
+    var answerBtn4 = $("#choice4");
+    var feedbackEl = $("#feedback");
+    var getStartedBtn = $("#getStarted");
+    var currentQuestion = 0;
+    //create timer
+    var timer = 200;
+    var timerCountdownEl = $("#timerCountdown");
+    var timerReference = undefined;
 
-getStartedBtn.click(function (event) {
-    //this picks the first question of the questions array and puts it in the local variable, question1. 
-    var question1 = questions[0];
-    //this makes the h3 text, var question1's question value.
-    questionTxtEl.text(question1.question);
-    //create a local variable with the choices of question 1. 
-    var question1choices = question1.choices;
-    //fill in each button with a choice from the array
-    answerBtn1.text(question1choices[0]);
-    answerBtn2.text(question1choices[1]);
-    answerBtn3.text(question1choices[2]);
-    answerBtn4.text(question1choices[3]);
+    welcomeBox.show();
+    questionBox.hide();
+    endingScoreBox.hide();
+    highScoresBox.hide();
+
+    //when the page loads, the questions get shuffled.
+    for (let i = questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        const temp = questions[i];
+        questions[i] = questions[j];
+        questions[j] = temp;
+    }
+    //when you click on getStarted, the welcomeBox hides, and first rando question shows.
+    getStartedBtn.click(function (event) {
+        //hide various boxes and show question box.
+        welcomeBox.hide();
+        questionBox.show();
+        //this picks the first question of the questions array and puts it in the local variable, question1. 
+        var question1 = questions[0];
+        //this makes the h3 text, var question1's question value.
+        questionTxtEl.text(question1.question);
+        //create a local variable with the choices of question 1. 
+        var question1choices = question1.choices;
+        //fill in each button with a choice from the array
+        answerBtn1.text(question1choices[0]);
+        answerBtn2.text(question1choices[1]);
+        answerBtn3.text(question1choices[2]);
+        answerBtn4.text(question1choices[3]);
+        //start countdown
+        timerReference = window.setInterval(function () {
+            timer--;
+            if (timer < 0) {
+                //showScore();
+            } else {
+                timerCountdownEl.text(timer);
+            };
+        }, 1000);
+
+    });
+    //when an answerBtn is clicked, it is either correct or false. If correct, feedback=Correct!, pause for 1 second, and continue.
+    $(".answer").on("click", function (event) {
+        event.preventDefault();
+        var correctAnswer = questions[currentQuestion].answer;
+        var theirAnswer = event.target.innerText;
+        if (theirAnswer == correctAnswer) {
+            feedbackEl.text("Correct!").show();
+        } else {
+            //deduct 10 seconds, and go to next question.
+            feedbackEl.text("Wrong!").show();
+            timer -= 10;
+        }
+        //pause for 1 second, then go to next question.
+        window.setTimeout(function () {
+            //goes to next question
+            showNextQuestionOrScore();
+        }, 1000);
+    });
+
+    function showNextQuestionOrScore() {
+        currentQuestion++;
+        if (currentQuestion == questions.length) {
+            questionBox.hide();
+            endingScoreBox.show();
+            $("#endingScore").text("Ending Score: " + timer);
+            window.clearInterval(timerReference);
+        } else {
+            var question1 = questions[currentQuestion];
+            //this makes the h3 text, var question1's question value.
+            questionTxtEl.text(question1.question);
+            //create a local variable with the choices of question 1. 
+            var question1choices = question1.choices;
+            //fill in each button with a choice from the array
+            answerBtn1.text(question1choices[0]);
+            answerBtn2.text(question1choices[1]);
+            answerBtn3.text(question1choices[2]);
+            answerBtn4.text(question1choices[3]);
+        }
+    }
 
 });
 
-//shuffle the questions array
-var randoQuestion = shuffle(questions);
 
-//create timer
-var timer = {
-    time= 2000,
-
-}
-
-
-
-
-
-
-
-
-//when an answerBtn is clicked, it is either correct or false. If correct, feedback=Correct! and add 10 points to score. 
-
-
-
-//If incorrect, feedback=Wrong! subtract 10 points, pause for 1 second, and move to next question.
-
-
-//If timer runs out, feedback=Time's up!, pause for 1 second, and end the quiz. Calculate the final score (number of questions right *10 + time in seconds left).
-
-//Final Score === current score up in the corner. The container has a text area that holds your initials. Enter initals and hit enter.
+//If timer runs out, feedback=Time's up!, pause for 1 second, and end the quiz. 
 
 //High Scores container has an ordered list that orders the array of high scores by HIGH -> LOW
 
